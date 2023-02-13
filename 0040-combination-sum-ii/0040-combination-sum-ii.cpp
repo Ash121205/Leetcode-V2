2a5b211da3f8c2d5dadff3195aa7270851113972
Ash121205 <78997375+Ash121205@users.noreply.github.com>
2023-02-13 16:21:02 +0530

    Time: 10 ms (42.13%), Space: 10.7 MB (68.29%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,27 +1,23 @@
 class Solution {
-public:
-    void findans(vector<int>& candidates, int target, int sum, vector<vector<int>>& ans, vector<int>& ds, int index){
-        if(target == sum){
+    public: 
+    void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int>&ds) {
+        if(target==0) {
             ans.push_back(ds);
             return;
+        }        
+        for(int i = ind;i<arr.size();i++) {
+            if(i>ind && arr[i]==arr[i-1]) continue; 
+            if(arr[i]>target) break; 
+            ds.push_back(arr[i]);
+            findCombination(i+1, target - arr[i], arr, ans, ds); 
+            ds.pop_back(); 
         }
-        if(candidates.size() == index)
-            return;
-        if(sum > target)
-            return;
-        ds.push_back(candidates[index]);
-        findans(candidates, target, sum + candidates[index], ans, ds, index + 1);
-        ds.pop_back();
-        while(index < candidates.size()-1 && candidates[index] == candidates[index + 1])
-            index++;
-        findans(candidates, target, sum, ans, ds, index + 1);
     }
-
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> ds;
         sort(candidates.begin(), candidates.end());
-        findans(candidates, target, 0, ans, ds, 0);
-        return ans;
+        vector<vector<int>> ans; 
+        vector<int> ds; 
+        findCombination(0, target, candidates, ans, ds); 
+        return ans; 
     }
 };
