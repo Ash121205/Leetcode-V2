b4fc48213d9686c4999c076005da73f298c26684
Ash121205 <78997375+Ash121205@users.noreply.github.com>
2023-03-12 08:21:48 +0530

    Time: None, Memory: None',   langs: [
      ],
      runtime: '243',
      memory: '88628000',
      enableMemoryDistribution: 'True',
      nonSufficientMsg: 'None - LeetHub

--- undefined/undefined.cpp
+++ undefined/undefined.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int minimizeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
+    int maxScore(vector<int>& nums) {
+        sort(nums.rbegin(), nums.rend());
         
-//         if(nums[0] == nums[n-1]) return 0;
-//         else if((nums[1] == nums[n-1]) || (nums[0] == nums[n-2])) return abs(nums[n-1]-nums[0]);
-//         // else if() return nums[n-1]-nums[0];
+        vector<long long>pf(nums.size());
         
-//         else{
-//             int l = abs(nums[2] - nums[0]);
-//             int h = abs(nums[n-1] - nums[3]);
-//             return min(l+h, abs(nums[n-1]-nums[1])) - 1;
-        
-        long long ans = min((nums[n-3]-nums[0]), min((nums[n-2]-nums[1]), nums[n-1]-nums[2]));
+        pf[0] = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            pf[i] = pf[i-1] + nums[i];
+        }
+        long long ans = 0;
+        for (int i=0;i<pf.size();i++) {
+            if (pf[i] > 0) {
+                ans++;
+            }
+        }
         return ans;
+        
     }
 };
